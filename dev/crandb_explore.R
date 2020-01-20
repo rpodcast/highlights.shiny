a231589b1abe3e4a08a4d01252c0df534eea68a0
Eric Nantz <thercast@gmail.com>
2020-01-20 14:23:24 -0500

    polish scraping code to get intermediate result

--- dev/crandb_explore.R
+++ dev/crandb_explore.R
@@ -5,6 +5,8 @@ library(cranly)
 library(cranlogs)
 library(rvest)
 library(polite)
+library(janitor)
+
 p_db <- tools::CRAN_package_db()
 package_db <- cranly::clean_CRAN_db(p_db)
 
@@ -20,14 +22,16 @@ shiny_rev_suggests <- package_db %>%
   pull(reverse_suggests) %>%
   .[[1]]
 
+shiny_pkg_list <- shiny_rev_imports
+
 # subset package db with just these shiny-related packages
 shiny_rev_db <- package_db %>%
-  filter(package %in% c(shiny_rev_imports, shiny_rev_suggests))
+  filter(package %in% shiny_pkg_list)
 
 # see how RStudio is captured in author field
-shiny_rev_db %>%
-  filter(package == "shinydashboard") %>%
-  pull(author)
+# shiny_rev_db %>%
+#   filter(package == "shinydashboard") %>%
+#   pull(author)
 
 # create flag for whether RStudio is part of author or not
 shiny_rev_db <- shiny_rev_db %>%
@@ -36,7 +40,12 @@ shiny_rev_db <- shiny_rev_db %>%
   }))
 
 # grab RStudio-authored package names related to Shiny in some way
-filter(shiny_rev_db, rstudio_author) %>% pull(package)
+#filter(shiny_rev_db, rstudio_author) %>% pull(package)
+
+# prepare a meta dataframe for holding release information
+shiny_rev_db <- shiny_rev_db %>%
+  filter(!rstudio_author) %>%
+  select(package, title, description, author, published)
 
 # begin assembling URLs to scrape all release dates / versions of packages
 cran_bow <- bow(
@@ -45,11 +54,15 @@ cran_bow <- bow(
   force = TRUE
 )
 
-cran_bow
-
-scrape_pkg <- function(pkgname, bow = cran_bow, base_url = 'https://cran.r-project.org/src/contrib/Archive') {
+scrape_pkg <- function(pkgname, 
+                       bow = cran_bow, 
+                       base_url = 'https://cran.r-project.org/src/contrib/Archive',
+                       verbose = TRUE) {
   
   # 1. Agree modification of session path with host
+  if (verbose) message(glue::glue("{time}: Beginning release scrape for {pkgname}....",
+                                  time = Sys.time()))
+  
   session <- nod(
     bow = bow,
     path = glue::glue("src/contrib/Archive/{pkgname}")   #paste0("r/", subreddit_name)
@@ -58,10 +71,43 @@ scrape_pkg <- function(pkgname, bow = cran_bow, base_url = 'https://cran.r-proje
   # 2. Scrape the page from the new path
   scraped_page <- scrape(session)
   
+  # if the page does not exist, it means the package only had one release (the date in the cran db)
+  if (is.null(scraped_page)) {
+    stop("Package only has 1 release", call. = FALSE)
+  }
+  
   # 3. Extract from xpath on the altered URL
   node_result <- html_nodes(scraped_page, "table")
   
-  return(node_result)
+  # obtain table and perform cleaning
+  df <- html_table(node_result[[1]], fill = TRUE) %>%
+    clean_names() %>%
+    remove_empty(c("rows", "cols")) %>%
+    filter(name != "") %>%
+    filter(name != "Parent Directory") %>%
+    tibble::rownames_to_column(var = "release_number")
+  
+  # create summary with one row (first release date, number of releases)
+  n_releases <- slice(df, n()) %>% pull(release_number) %>% as.integer(.)
+  first_release_date <- slice(df, 1L) %>% pull(last_modified)
+  last_release_date <- slice(df, n()) %>% pull(last_modified)
+  
+  res <- tibble::tibble(
+    package = pkgname,
+    n_releases = n_releases,
+    first_release_date = first_release_date,
+    last_release_date = last_release_date
+  )
+  
+  return(res)
 }
 
-scrape_pkg("shiny")
+scrape_pkg("ABACUS")
+
+# run function with purrr
+safely_scrape_pkg <- purrr::safely(scrape_pkg)
+
+full_res <- purrr::map(shiny_pkg_list, ~safely_scrape_pkg(.x))
+
+saveRDS(full_res, file = "dev/full_res.rds")
+saveRDS(shiny_rev_db, file = "dev/shiny_rev_db.rds")

edfbee9daf22cf3abd77820dac0af0b1c891e77a
Eric Nantz <thercast@gmail.com>
2020-01-20 23:30:35 -0500

    start fullPage UI

--- R/app_ui.R
+++ R/app_ui.R
@@ -1,11 +1,55 @@
 #' @import shiny
+#' @import fullPage
 app_ui <- function() {
+  
+  my_options <- list(
+    loopBottom = TRUE,
+    navigation = TRUE,
+    keyboardScrolling = TRUE
+  )
+  
+  sections.color = c(
+    "#C3F3EC",
+    "#FE9199",
+    "#FFADB2",
+    "#FFCAB1",
+    "#C3F3EC"
+  )
+  
+  
   tagList(
     # Leave this function for adding external resources
-    golem_add_external_resources(),
+    #golem_add_external_resources(),
+    
     # List the first level UI elements here 
-    fluidPage(
-      h1("hightlights.shiny")
+    pagePiling(
+      center = TRUE,
+      sections.color = sections.color,
+      opts = my_options,
+      menu = c(
+        "History" = "intro",
+        "Architect" = "dev",
+        "User Interface" = "ui_pkgs",
+        "User Experience" = "ux_pkgs",
+        "The Future" = "shiny_future"
+      ),
+      
+      pageTheme("aqua"),
+      
+      pageSection(
+        menu = "intro",
+        h1("Highlights of the Shiny Community!"),
+        h2("Eric Nantz"),
+        h2("Statistician / Podcaster / Shiny Enthusiest"),
+        h2("rstudio::conf 2020"),
+        br(),
+        br(),
+        pageContainer(
+          pageRow(
+            pageButtonTo(tags$p(fontawesome::fa("car", height = 12, fill = "forestgreen"), "Hello"), section = "intro")
+          )
+        )
+      )
     )
   )
 }
@@ -13,9 +57,9 @@ app_ui <- function() {
 #' @import shiny
 golem_add_external_resources <- function(){
   
-  addResourcePath(
-    'www', system.file('app/www', package = 'hightlights.shiny')
-  )
+  # addResourcePath(
+  #   'www', system.file('app', 'www', package = 'hightlights.shiny')
+  # )
  
   tags$head(
     golem::activate_js(),
